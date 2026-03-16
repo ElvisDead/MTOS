804491b35431dcae7cace19604c28342a407d03f
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-16 17:34:38 +0200

    Create mapsConfig.js

---
+++ js/mapsConfig.js
@@ -0,0 +1,19 @@
+export const MTOS_MAPS = {
+
+pressure: () => pyodide.runPython(`mtos_pressure_map()`).toJs(),
+
+gradient: () => pyodide.runPython(`mtos_pressure_gradient()`).toJs(),
+
+atlas: () => pyodide.runPython(`mtos_climate_atlas()`).toJs(),
+
+attractor: () => pyodide.runPython(`mtos_attractor_map(name,year,month,day)`).toJs(),
+
+phase: () => pyodide.runPython(`mtos_phase_matrix()`).toJs(),
+
+wave: () => pyodide.runPython(`mtos_wave_structure()`).toJs(),
+
+tzolkin: () => pyodide.runPython(`mtos_tzolkin_structure()`).toJs(),
+
+geometry: () => pyodide.runPython(`mtos_tzolkin_geometry()`).toJs()
+
+}
