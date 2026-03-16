94f8e2f2914e4acf5f17c217a897b0a21df5a3bf
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-16 17:32:52 +0200

    Update tzolkin.js

--- js/tzolkin.js
+++ js/tzolkin.js
@@ -2,7 +2,26 @@ export function analog(i){
 return (i + 4) % 20
 }
 
+function kinFromTS(tone,seal){
 
+return ((seal*13)+tone)%260 +1
 
+}
 
+function tsFromKin(kin){
 
+let tone=(kin-1)%13
+let seal=(kin-1)%20
+
+return {tone,seal}
+
+}
+
+function kinToTS(kin){
+
+let tone=(kin-1)%13
+let seal=(kin-1)%20
+
+return {tone,seal}
+
+}
