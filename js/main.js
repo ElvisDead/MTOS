a8a466aaff3ff37e74b7c2ebc60b542446bd6f29
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-16 17:12:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import { drawChart } from "./charts.js"
 
 window.drawChart = drawChart
+
+import { getColor, getColorInferno } from "./colors.js"
+
+window.getColor = getColor
+window.getColorInferno = getColorInferno
