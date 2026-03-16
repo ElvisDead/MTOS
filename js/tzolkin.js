2cbde005a18df0b6d4ddc64f25e5c959a05c8c13
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-16 18:09:35 +0200

    Update tzolkin.js

--- js/tzolkin.js
+++ js/tzolkin.js
@@ -1,3 +1,30 @@
+export const tones = [
+1,2,3,4,5,6,7,8,9,10,11,12,13
+]
+
+export const seals = [
+"Dragon",
+"Wind",
+"Night",
+"Seed",
+"Serpent",
+"WorldBridger",
+"Hand",
+"Star",
+"Moon",
+"Dog",
+"Monkey",
+"Human",
+"Skywalker",
+"Wizard",
+"Eagle",
+"Warrior",
+"Earth",
+"Mirror",
+"Storm",
+"Sun"
+]
+
 export function analog(i){
 return (i + 4) % 20
 }
