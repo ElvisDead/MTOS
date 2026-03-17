c66376858d4302b2d8f4408c6c388dc9cca9bbc8
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-17 14:33:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ import { renderMap } from "./renderMap.js"
 import { drawPhaseSpace } from "./phaseSpace.js"
 import { exportExperiment } from "./exportExperiment.js"
 import { drawKinMap } from "./kinmap.js"
-window.drawKinMap = drawKinMap
 import { kinFromTS, tsFromKin, kinToTS } from "./tzolkin.js"
 import { MTOS_MAPS } from "./mapsConfig.js"
 import { run } from "./run.js"
