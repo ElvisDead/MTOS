c722522847ef3d920d767fe04f8576449fc2a898
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-17 13:31:02 +0200

    Update kinmap.js

--- js/kinmap.js
+++ js/kinmap.js
@@ -142,13 +142,6 @@ popup.style.whiteSpace = "pre-line"
 
 popup.innerHTML = `
 
-document.body.appendChild(popup)
-
-}
-
-map.appendChild(c)
-cells.push(c)
-
 <div style="font-size:16px;margin-bottom:6px">
 <b>Kin ${kin}</b>
 </div>
@@ -194,8 +187,12 @@ ${userList}
 </div>
 
 `
+document.body.appendChild(popup)
 
 }
+
+map.appendChild(c)
+cells.push(c)
 
 }
 
