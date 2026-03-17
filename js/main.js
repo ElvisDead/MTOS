dca70290ee512b2803f72e6b22b207c6b5c3d99e
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-17 22:33:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ import { renderMap } from "./renderMap.js"
 import { drawPhaseSpace } from "./phaseSpace.js"
 import { exportExperiment } from "./exportExperiment.js"
 import { drawKinMap } from "./kinmap.js"
+import { drawGlobalKinMap } from "./globalKinMap.js"
 import { kinFromTS, tsFromKin, kinToTS } from "./tzolkin.js"
 import { MTOS_MAPS } from "./mapsConfig.js"
 import { run } from "./run.js"
 import { startMTOS } from "./pyodideLoader.js"
+
 
 window.addEventListener("DOMContentLoaded", () => {
   startMTOS()
