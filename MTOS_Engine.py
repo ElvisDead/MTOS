4ccb4d8e51478a53e4ba27f2850207c462023e0a
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-18 16:36:57 +0200

    Update MTOS_Engine.py

--- MTOS_Engine.py
+++ MTOS_Engine.py
@@ -825,7 +825,7 @@ def mtos_series(name,year,month,day,days=30):
 
     series=simulate(i,tone,today,days)
 
-    return json.dumps(series.tolist())
+    return series.tolist()
 
 def mtos_260_weather(name,year,month,day):
 
@@ -836,7 +836,7 @@ def mtos_260_weather(name,year,month,day):
         key = f"{name}_{today}"
 
         if key in cache:
-            return json.dumps(cache[key])
+            return cache[key]
 
     birth=datetime.date(year,month,day)
 
@@ -977,7 +977,7 @@ def mtos_260_weather(name,year,month,day):
         "field": field.tolist()
     })
 
-    return json.dumps(weather)
+    return weather
 
 def mtos_user_climate(user_seal):
 
@@ -1187,10 +1187,10 @@ def mtos_phase_space(name,year,month,day):
         xs.append(float(x))
         ys.append(float(y))
 
-    return json.dumps({
+    return {
         "x":xs,
         "y":ys
-    })
+    }
 
 def mtos_phase_density(name,year,month,day):
 
