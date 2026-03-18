5d8e72ff8a407a226b3b89ca8b7d138905c5bbb1
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-18 18:00:16 +0200

    Update pressureMap.js

--- js/pressureMap.js
+++ js/pressureMap.js
@@ -1,11 +1,13 @@
 export function drawPressureMap(id, data){
 
     const c = document.getElementById(id)
-    c.innerHTML=""
+    if(!c) return
 
-    c.style.display="grid"
-    c.style.gridTemplateColumns="repeat(13,16px)"
-    c.style.gap="2px"
+    c.innerHTML = ""
+
+    c.style.display = "grid"
+    c.style.gridTemplateColumns = "repeat(13, 14px)"
+    c.style.gap = "2px"
 
     for(let i=0;i<data.length;i++){
 
@@ -15,9 +17,10 @@ export function drawPressureMap(id, data){
         const b = Math.floor(255*(1-v))
 
         const cell = document.createElement("div")
-        cell.style.width="16px"
-        cell.style.height="16px"
-        cell.style.background=`rgb(${r},0,${b})`
+
+        cell.style.width = "14px"
+        cell.style.height = "14px"
+        cell.style.background = `rgb(${r},0,${b})`
 
         c.appendChild(cell)
     }
