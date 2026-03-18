4a1eff255747cb2cbe183f6a00c9c81d38454d09
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-18 18:00:57 +0200

    Create phaseMap.js

---
+++ js/phaseMap.js
@@ -0,0 +1,26 @@
+export function drawPhaseMap(id, data){
+
+    const c = document.getElementById(id)
+    if(!c) return
+
+    c.innerHTML = ""
+
+    c.style.display = "grid"
+    c.style.gridTemplateColumns = "repeat(20, 14px)"
+    c.style.gap = "2px"
+
+    for(let i=0;i<data.length;i++){
+
+        const v = data[i]
+
+        const gray = Math.floor(255*v)
+
+        const cell = document.createElement("div")
+
+        cell.style.width = "14px"
+        cell.style.height = "14px"
+        cell.style.background = `rgb(${gray},${gray},${gray})`
+
+        c.appendChild(cell)
+    }
+}
