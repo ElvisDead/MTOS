0fcbff7acfef8fcb7018ec04873c34a614bc61eb
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-18 10:43:22 +0200

    Update renderMap.js

--- js/renderMap.js
+++ js/renderMap.js
@@ -1,6 +1,8 @@
 import { drawHeatmap } from "./heatmap.js"
 
 export function renderMap(id, cols=20, rows=13){
+console.log("DRAW MAP:", id)
+
 if(!MTOS_MAPS[id]) return
 
 let data = MTOS_MAPS[id]()
