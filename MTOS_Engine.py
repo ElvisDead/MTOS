ad83a0c4aa027725335d6a6b3718b7733516cce0
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-23 12:04:51 +0200

    Update MTOS_Engine.py

--- MTOS_Engine.py
+++ MTOS_Engine.py
@@ -964,29 +964,30 @@ def mtos_260_weather(name,year,month,day):
 
         }
 
-    # 🔥 ДОСТРАИВАЕМ ПРОПУЩЕННЫЕ
-    full_weather = [None]*260
+        # 🔥 ДОСТРАИВАЕМ ПРОПУЩЕННЫЕ
+        full_weather = [None]*260
 
-    for i in range(0,260,4):
-        full_weather[i] = weather[i]
-        
-        for i in range(260):
-            if full_weather[i] is None:
-                left = (i//4)*4
-                right = min(left+4, 259)
-                full_weather[i] = full_weather[left] or full_weather[right]
-                
-                weather = full_weather
+        for i in range(0,260,4):
+            full_weather[i] = weather[i]
+            
+            for i in range(260):
+                if full_weather[i] is None:
+                    left = (i//4)*4
+                    right = min(left+4, 259)
+                    full_weather[i] = full_weather[left] or full_weather[right]
+                    
+                    weather = full_weather
+                    
+                    if USE_CACHE:
+                        
+                        cache[key] = weather
+                        
 
-    if USE_CACHE:
-
-        cache[key] = weather
-
-        # ограничение размера
-        if len(cache) > 50:
-            cache.pop(next(iter(cache)))
-
-        save_weather_cache(cache)
+                        # ограничение размера
+                        if len(cache) > 50:
+                            cache.pop(next(iter(cache)))
+                            
+                            save_weather_cache(cache)
 
     # ==========================================
     # FIELD DYNAMICS WITH TIME (NEW)
