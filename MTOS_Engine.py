76a3c8718fb3d51da642f03bfc9522aadfdf7ad5
LvsD <71656308+ElvisDead@users.noreply.github.com>
2026-03-23 11:57:52 +0200

    Update MTOS_Engine.py

--- MTOS_Engine.py
+++ MTOS_Engine.py
@@ -965,18 +965,18 @@ def mtos_260_weather(name,year,month,day):
         }
 
     # 🔥 ДОСТРАИВАЕМ ПРОПУЩЕННЫЕ
-full_weather = [None]*260
+    full_weather = [None]*260
 
-for i in range(0,260,4):
-    full_weather[i] = weather[i]
-
-for i in range(260):
-    if full_weather[i] is None:
-        left = (i//4)*4
-        right = min(left+4, 259)
-        full_weather[i] = full_weather[left] or full_weather[right]
-
-weather = full_weather
+    for i in range(0,260,4):
+        full_weather[i] = weather[i]
+        
+        for i in range(260):
+            if full_weather[i] is None:
+                left = (i//4)*4
+                right = min(left+4, 259)
+                full_weather[i] = full_weather[left] or full_weather[right]
+                
+                weather = full_weather
 
     if USE_CACHE:
 
